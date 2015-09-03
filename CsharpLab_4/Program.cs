afb3145002bc95d6be39d501bc4512e732cecd69
Andry Yushko <andrjj.jushko@gmail.com>
2015-09-03 12:14:12 +0300

    Hi

---
+++ CsharpLab_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            B obj = new B(k);
+
+
+            foreach (int index in obj.mas)
+                Console.WriteLine(index);
+            Console.ReadKey();
+
+        }
+    }
+
+    class B
+    {
+        int a, b, d;
+        public int[] mas;
+
+        public B()
+        {
+            Console.Write("a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("d = ");
+            d = Convert.ToInt32(Console.ReadLine());
+            mas = new int[a];
+        }
+
+        public B(decimal k)
+            : this()
+        {
+            mas = new int[a];
+
+            for (int index = 0; index < a; index++)
+                mas[index] = c2 * index;
+
+        }
+
+
+        public int c2
+        {
+            get
+            {
+                while (d <= 10) d += a + b;
+
+                return d;
+
+            }
+            set
+            {
+                d = value;
+
+            }
+        }
+    }
+}
